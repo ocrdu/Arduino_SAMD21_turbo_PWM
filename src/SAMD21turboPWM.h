d0f3bb6741bed773cbaf0bb37d459b84f3e5c2c5
ocrdu <oscar@den-uijl.nl>
2019-12-08 13:28:20 +0100

    Minor edits

--- src/SAMD21turboPWM.h
+++ src/SAMD21turboPWM.h
@@ -10,15 +10,15 @@ class TurboPWM {
     void enable(unsigned int timerNumber, bool enabled);
     float frequency(unsigned int timerNumber);
   private:
-    unsigned int _GCLKDiv = 1;        // Main clock divider: 1 to 255 for both TCC0 and TCC1
-    unsigned int _TCCDiv0 = 1;        // TTC0 clock divider: 1, 2, 4, 8, 16, 64, 256, or 1024
-    unsigned int _TCCDiv1 = 1;        // TTC1 clock divider: 1, 2, 4, 8, 16, 64, 256, or 1024
-    unsigned long int _sts0 = 500000; // Timer 0 PWM steps (resolution): 2 to 0xFFFFFF (24 bits)
-    unsigned long int _sts1 = 500000; // Timer 1 PWM steps (resolution): 2 to 0xFFFFFF (24 bits)
-    bool _fastPWM0 = false;           // False for phase-correct aka dual-slope PWM, true for fast aka normal aka single-slope PWM
-    bool _fastPWM1 = false;           // False for phase-correct aka dual-slope PWM, true for fast aka normal aka single-slope PWM
-    bool _enabled0 = true;            // Shows if TCC0 is enabled
-    bool _enabled1 = true;            // Shows if TCC1 is enabled
+    unsigned int _GCLKDiv = 1;         // Main clock divider: 1 to 255 for both TCC0 and TCC1
+    unsigned int _TCCDiv0 = 1;         // TTC0 clock divider: 1, 2, 4, 8, 16, 64, 256, or 1024
+    unsigned int _TCCDiv1 = 1;         // TTC1 clock divider: 1, 2, 4, 8, 16, 64, 256, or 1024
+    unsigned long int _sts0 = 500000;  // Timer 0 PWM steps (resolution): 2 to 0xFFFFFF (24 bits)
+    unsigned long int _sts1 = 500000;  // Timer 1 PWM steps (resolution): 2 to 0xFFFFFF (24 bits)
+    bool _fastPWM0 = false;            // False for phase-correct aka dual-slope PWM, true for fast aka normal aka single-slope PWM
+    bool _fastPWM1 = false;            // False for phase-correct aka dual-slope PWM, true for fast aka normal aka single-slope PWM
+    bool _enabled0 = true;             // Shows if TCC0 is enabled
+    bool _enabled1 = true;             // Shows if TCC1 is enabled
     bool _turbo = false;
 };
 
@@ -30,8 +30,8 @@ typedef struct {
   unsigned long int pMux;
 } PinLookup;
 
-#if defined(ARDUINO_SAMD_NANO_33_IOT)
-static const PinLookup pinTable[] = {  // This is the pin table for the Arduino Nano 33 IOT
+static const PinLookup pinTable[] = {
+#if defined(ARDUINO_SAMD_NANO_33_IOT)  // This is the pin table for the Arduino Nano 33 IOT
 {-1},
 {-1}, 
 {-1}, 
@@ -45,10 +45,9 @@ static const PinLookup pinTable[] = {  // This is the pin table for the Arduino 
 {-1}, 
 {-1}, 
 {-1},
-{13, PORTA, 17, 0x03, PORT_PMUX_PMUXO_F}};
+{13, PORTA, 17, 0x03, PORT_PMUX_PMUXO_F}
 
-#elif defined(ARDUINO_SAMD_MKR1010)
-static const PinLookup pinTable[] = {  // This is the pin table for the Arduino MKR WiFi 1010
+#elif defined(ARDUINO_SAMD_MKR1010)    // This is the pin table for the Arduino MKR WiFi 1010
 {-1},
 {-1}, 
 { 2, PORTA, 10, 0x10, PORT_PMUX_PMUXE_E}, 
@@ -56,8 +55,9 @@ static const PinLookup pinTable[] = {  // This is the pin table for the Arduino 
 { 4, PORTB, 10, 0x00, PORT_PMUX_PMUXE_F},
 { 5, PORTB, 11, 0x01, PORT_PMUX_PMUXO_F},
 { 6, PORTA, 20, 0x02, PORT_PMUX_PMUXE_F},
-{ 7, PORTA, 21, 0x03, PORT_PMUX_PMUXO_F}};
+{ 7, PORTA, 21, 0x03, PORT_PMUX_PMUXO_F}
 
 #endif
+};
 
 static const unsigned int pinTableSize = sizeof(pinTable) / sizeof(pinTable[0]);
